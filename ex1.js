c4734fe4c36553c2dff9ec09f06e9381be6b3087
Leonardo ALARCON <E13A237Z@U-INF-J-E113-02.iut-nantes.univ-nantes.prive>
2013-10-23 10:58:53 +0200

    Correction des erreurs

--- ex1.js
+++ ex1.js
@@ -1,24 +1,35 @@
+// Hello.
+//
+// This is JSHint, a tool that helps to detect errors and potential
+// problems in your JavaScript code.
+//
+// To start, simply enter some JavaScript anywhere on this page. Your
+// report will appear on the right side.
+//
+// Additionally, you can toggle specific options in the Configure
+// menu.
+
 function adder(){
 	var args = Array.prototype.slice.call(arguments);
 	var res = 0;
 	return function(v){
-      	args.forEach(function(val, i){
+      args.forEach(function(val, i){
 			res += val(v);
 		});
 		return res;
-   }
+   };
 }
 
 function mult(v){
   return function(e){
     return v*e;
-  }
+  };
 }
 
 function sub(a){
 	return function(e){
 		return a-e;
-	}
+	};
 }
 
 console.log(
